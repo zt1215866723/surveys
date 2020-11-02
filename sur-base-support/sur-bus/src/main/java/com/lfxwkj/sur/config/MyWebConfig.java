a8cf1c09a084978ae2c501903b1cd2a8532ee0a9
wangnanxiang <806543499@qq.com>
2020-11-02 15:47:46 +0800

    图例上传

---
+++ sur-base-support/sur-bus/src/main/java/com/lfxwkj/sur/config/MyWebConfig.java
@@ -0,0 +1,21 @@
+package com.lfxwkj.sur.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *  配置类
+ * @author 王南翔
+ * @since 2020-11-02
+ */
+
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+
+    //图片虚拟路径 防止本机图片被拦截
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/Users/wangnanxiang/Downloads/**").addResourceLocations("file:/Users/wangnanxiang/Downloads/");
+    }
+}
