d6b775e7be39b1e415c9364820a836fe625b990a
hansihao <1119675227@QQ.com>
2020-11-05 17:54:59 +0800

    111

--- sur-base-support/sur-sys/src/main/java/com/lfxwkj/sur/sys/modular/system/mapper/DeptMapper.java
+++ sur-base-support/sur-sys/src/main/java/com/lfxwkj/sur/sys/modular/system/mapper/DeptMapper.java
@@ -25,12 +25,12 @@ public interface DeptMapper extends BaseMapper<Dept> {
     /**
      * 获取ztree的节点列表
      */
-    List<ZTreeNode> tree(@Param("deptId") String deptId);
+    List<ZTreeNode> tree();
 
     /**
      * 获取layui树形节点
      */
-    List<LayuiTreeNode> layuiTree(String deptId);
+    List<LayuiTreeNode> layuiTree();
 
     /**
      * 获取所有部门列表
